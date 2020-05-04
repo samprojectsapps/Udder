e3e582780cf532830610a6abe4fd7c5e58f9ef17
Samprojects <samprojectsapps@gmail.com>
2020-05-03 21:02:37 -0700

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Absalom Lwande on 5/3/20.
+//  Copyright © 2020 Absalom Lwande. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
